8cf4e0251d8d402082ecbfb1433abba491273526
Saeed Ganji <shahabganji@gmail.com>
2019-01-15 17:50:30 +0330

    chores( create & edit )
    Almost the initial structure is ready

--- src/contacts/contact-new.ts
+++ src/contacts/contact-new.ts
@@ -1,3 +1,39 @@
+import { autoinject } from "aurelia-framework";
+import { ContactsInMemoryService } from "./services/contacts-service";
+import { IContact } from "./models/contact";
+
+@autoinject()
 export class ContactNew {
+
+  private isNew: boolean = false;
+  private contact: IContact = null;
+
+  constructor(private contactService: ContactsInMemoryService) { }
+
+  activate(params) {
+    console.log(params);
+
+    if (params && params.id) {
+      this.contactService.getContact(Number(params.id))
+        .then(found => {
+          this.isNew = false;
+          this.contact = found;
+        });
+    }
+    else {
+      this.isNew = true;
+      this.clearContact();
+    }
+  }
+
+  private clearContact(): void {
+    this.contact = {
+      id: -1,
+      name: '',
+      family: '',
+      email: '', website: '', phone: ''
+    };
+  }
+
 
 }
